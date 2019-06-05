852091aab52e2c5ceb9d6516f86f6fafd33329b9
Hector <mamami1991@hotmail.com>
2019-06-05 16:50:33 +0800

    pytesseract解析验证码

---
+++ demo/0-练习demo/0.8-pytesseract解析验证码/main.py
@@ -0,0 +1,28 @@
+# 0.8-pytesseract解析验证码
+
+# 1.安装依赖库Pillow(图像处理库)
+# $ pip3 install Pillow
+
+# 2.安装tesseract-ocr(google的ocr识别引擎)
+# https://github.com/tesseract-ocr/tesseract/wiki 下载windows安装包exe，如果需要识别汉字，在安装时勾选语言包
+# 安装完之后，配置环境变量，$ tesseract -v 可查看版本号
+# $ tesseract [目标图片] [输出文本] [-l 语言代码] 可在cmd中进行简单操作
+
+# 3.安装py识别引擎pytesseract https://pypi.org/project/pytesseract/0.1/
+# $ pip install pytesseract
+
+# 引入pillow图形处理库
+from PIL import Image
+import pytesseract
+
+
+# 打开测试图片
+im = Image.open("./test.png")
+vcode = pytesseract.image_to_string(im)
+# vcode = pytesseract.image_to_string(im, lang="chi_sim") # lang 语言
+
+print(vcode)
+# 准确度很低，优化方案：
+# 1.通过训练模型来增强
+# 2.彩色转灰度，灰度转二值，二值图像识别
+
